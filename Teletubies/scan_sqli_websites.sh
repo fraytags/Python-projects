097e00fd8e9716c0d29f0eddd606259d75689878
fraytags <dev@malotru.localdomain>
2024-04-09 00:14:10 +0200

    ajout du script scrapper ethical only

--- Teletubies/scan_sqli_websites.sh
+++ Teletubies/scan_sqli_websites.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-#
+# A simple shell script that launches sqlmap to scrap for vulnerable websites (ethical use only please), please contact the owners of the website if anything has been found
+# Fraytags 11/2021
+
 echo LANCEMENT DU SCRIPT
-cd /root/sqlmap 
 
 for i in `seq 1 10` ;
 do
